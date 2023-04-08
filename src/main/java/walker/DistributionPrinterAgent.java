18396775867915a07d403f6ca2fa36093499113c
David <david.cohen@studio.unibo.it>
2023-04-08 17:40:41 +0200

    Update code

--- src/main/java/walker/DistributionPrinterAgent.java
+++ src/main/java/walker/DistributionPrinterAgent.java
@@ -3,22 +3,18 @@ package walker;
 import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class DistributionPrinter implements Runnable {
+public class DistributionPrinterAgent implements Runnable {
     private final DirectoryWalkerParams params;
     private final AtomicBoolean isPrinting = new AtomicBoolean(false);
     private final int intervalInSeconds;
-    private final ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
     private List<Pair<Integer, Integer>> intervalRanges;
     private boolean isIntervalRangesDirty = true;
 
-    public DistributionPrinter(DirectoryWalkerParams params, int intervalInSeconds) {
+    public DistributionPrinterAgent(DirectoryWalkerParams params, int intervalInSeconds) {
         this.params = params;
         this.intervalInSeconds = intervalInSeconds;
     }
@@ -45,35 +41,44 @@ public class DistributionPrinter implements Runnable {
     }
 
     public void startPrinting() {
-        if (isPrinting.get()) {
-            // Already printing
-            return;
-        }
-
+//        if (isPrinting.get()) {
+//            // Already printing
+//            return;
+//        }
+        
         isPrinting.set(true);
-        executorService.scheduleAtFixedRate(this, 0, intervalInSeconds, TimeUnit.SECONDS);
+        new Thread(() -> {
+            try {
+                while (isPrinting.get()) {
+                    Thread.sleep(intervalInSeconds);
+                }
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     public void stopPrinting() {
         isPrinting.set(false);
-        executorService.shutdown();
     }
 
     @Override
     public void run() {
-        System.out.println(this.getDistributionString());
+        synchronized (System.out) {
+            System.out.println(this.getDistributionString());
+        }
     }
 
     public String getDistributionString() {
         synchronized (this.params.getDistribution()) {
-            updateIntervalRangesIfNeeded();
+            this.updateIntervalRangesIfNeeded();
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < intervalRanges.size(); i++) {
                 int start = intervalRanges.get(i).getLeft();
                 int end = intervalRanges.get(i).getRight();
                 List<Path> list = this.params.getDistribution().readDistribution().getOrDefault(i, Collections.emptyList());
                 if (start == this.params.getMaxLines()) {
-                    sb.append("[").append(start).append(",+∞]: ").append(list.size()).append("\n");
+                    sb.append("[").append(start).append(",+inf]: ").append(list.size()).append("\n");
                 } else {
                     sb.append("[").append(start).append(",").append(end).append("]: ").append(list.size()).append("\n");
                 }
